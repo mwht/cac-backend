079ef6d9a5d3a0abee9d0594f4b90ee5b11bd56a
Sebastian Madejski <madejski.s97@gmail.com>
2020-04-02 22:37:24 +0200

    add basic Spring scaffold

---
+++ src/main/java/pl/cardsagainstcards/CardAgainstCardsApplication.java
@@ -0,0 +1,13 @@
+package pl.cardsagainstcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardAgainstCardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardAgainstCardsApplication.class, args);
+    }
+
+}
